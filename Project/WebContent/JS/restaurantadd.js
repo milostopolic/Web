ffc2dfb2273ca762a6fe1ecf39be7cf03dd5e85a
milostopolic <topolicmilos@gmail.com>
2018-07-06 00:39:27 +0200

    uradjen admin cart u svim prozorima

--- Project/WebContent/JS/restaurantadd.js
+++ Project/WebContent/JS/restaurantadd.js
@@ -2,6 +2,7 @@ var restaurantadd_url = "../Project/webapi/restaurants"
 var articles_url = "../Project/webapi/articles"
 var loggeduser_url = "../Project/webapi/users/loggeduser"
 var logout_url = "../Project/webapi/users/logout"
+var loggedUser
 
 $(document).on("submit", "#restaurantaddForm", function(e) {
 	e.preventDefault();
@@ -37,6 +38,7 @@ function loadNavbar() {
 		
 		success : function(data) {
 			console.log(data.role);
+			loggedUser = data;
 			$("#navbarTogglerDemo03").empty();
 			$("#navbarTogglerDemo03").append(`<ul class="navbar-nav ml-auto mt-2 mt-lg-0"  id="listaID">			      
 				      
@@ -83,5 +85,181 @@ $(document).on('click', '#signOut', function(){
     	}});
 });
 
+function adminCartClick() {
+	$('#exampleModal').modal('toggle');
+	loadCartData();
+}
 
+function loadCartData() {
+	loadCartAdmin();
+	$.ajax({
+		type: 'GET',
+		url: "../Project/webapi/users",
+		contentType : 'application/json',
+		success: function(data) {
+			for(let us of data) {
+				if(us.role == 'CUSTOMER') {
+					$("#buyer").append(`<option value="`+us.username+`">`+us.username+`</option>`);
+				}
+				if(us.role == 'DELIVERY') {
+					if(us.vehicle == '')
+						$("#deliverer").append(`<option value="`+us.username+`">`+us.username+`</option>`);
+				}
+			}
+			
+			$.ajax({
+				type: 'GET',
+				url: "../Project/webapi/vehicles/freevehicles",
+				contentType : 'application/json',
+				success: function(data) {
+					for(let veh of data) {
+						$("#vehicles").append(`<option value="`+veh.model+`">`+veh.maker + ` `+ veh.model +`</option>`);
+					}
+					
+				},
+				error: function() {}
+				
+			});
+		},
+		error: function() {
+			
+		}
+	});
+}
+
+function loadCartAdmin(){
+	$("#modal-body").empty();
+	$("#modal-footer").empty();
+	if(loggedUser.cart.items.length > 0) {
+		$("#modal-body").append(`<table class="table">
+		  <thead class="thead-light">
+		    <tr>
+		      <th scope="col">Article</th>
+		      <th scope="col">Unit price</th>
+		      <th scope="col">Quantity</th>
+		      <th scope="col"></th>
+		    </tr>
+		  </thead>
+		  <tbody id="cartTable">
+		    
+		  </tbody>
+		</table>`);
+		for(let i = 0; i < loggedUser.cart.items.length; i++) {
+			$("#cartTable").append(`<tr>
+			<th>`+ loggedUser.cart.items[i].article +`</th><td>`+ loggedUser.cart.items[i].price +`</td>
+			<td><input style="max-width:50px" class="quantAdmin" id="` +loggedUser.cart.items[i].article + `" onkeydown="return false" min="1" max="10" type="number" value="`+loggedUser.cart.items[i].quantity +`"></td>
+			<td><img class="brisanjeCart" id="` +loggedUser.cart.items[i].article + `" style="margin-left:5px; cursor:pointer" height="24" witdh="24" src="images/delete.png" alt="appropriate alternative text goes here"></td>
+			</tr>`);
+		}
+		$("#modal-body").append(`<p>Total price: `+ loggedUser.cart.totalPrice +`</p>`);
+		$("#modal-body").append(`<div class="form-group row">
+				  <label for="buyer" class="col-2 col-form-label">Buyer</label>
+				  <div class="col-10">
+				    <select class="form-control" id="buyer">
+				  </div>
+				</div>`);
+		$("#modal-body").append(`<div class="form-group row">
+				  <label for="deliverer" class="col-2 col-form-label">Deliverer</label>
+				  <div class="col-10">
+				    <select class="form-control" id="deliverer">
+				  </div>
+				</div>`);
+		$("#modal-body").append(`<div class="form-group row">
+				  <label for="vehicles" class="col-2 col-form-label">Vehicle</label>
+				  <div class="col-10">
+				    <select class="form-control" id="vehicles">
+				  </div>
+				</div>`);
+		$("#modal-footer").append(`<input id="note" class="form-control mr-sm-2" type="text" placeholder="Note"/>`);
+		$("#modal-footer").append(`<button onclick="orderAdmin()" class="btn btn-success">Order</button>`);
+		
+	} else {
+		$("#modal-body").append(`<p>Your cart is empty.</p>`);
+	}
+	
+	
+}
+
+$(document).on('change', '.quantAdmin', function(){
+	var id = ($(this).attr('id'));
+	var q = ($(this).val());
+	console.log(q);	
+	$.ajax({
+		type: 'POST',
+		url: "../Project/webapi/orders/changequantity/" + id + "/" + q,
+		success: function() {
+			$.ajax({
+				type : 'GET',
+				url : loggeduser_url,
+		    contentType : 'application/json',
+		    
+				success : function(data) {
+					loggedUser = data;
+					loadCartData();
+				},
+				error: function() {
+					
+				}
+			});
+			
+		},
+		error:function(){
+			
+		}
+	})
+	
+});
+
+function orderAdmin() {
+	var veh = $("#vehicles").val();
+	$.ajax({
+		type : 'POST',
+		url : "../Project/webapi/orders/createadminorder/" + veh,
+    contentType : 'application/json',
+		dataType : "json",
+    data:orderToJSONAdmin(),
+		success : function(data) {
+			toastr.success("Order placed.");
+			loadNavbar();
+			$('#exampleModal').modal('toggle');
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+		}
+	});
+};
+
+function orderToJSONAdmin() {
+	return JSON.stringify({	
+    "buyer" : $("#buyer").val(),
+    "deliverer" : $("#deliverer").val(),
+	"note":$('#note').val(),
+    "items":loggedUser.cart.items,
+    "totalPrice":loggedUser.cart.totalPrice
+	});
+}
+
+$(document).on('click', '.brisanjeCart', function() {
+	var id = ($(this).attr('id'));	
+	$.ajax({
+		type: 'DELETE',
+		url : "../Project/webapi/orders/deleteitem/"+id,
+		contentType : 'application/json',
+		success: function() {
+			$.ajax({
+				type : 'GET',
+				url : loggeduser_url,
+		    contentType : 'application/json',
+		    
+				success : function(data) {
+					loggedUser = data;
+					loadCartAdmin();
+				},
+				error: function() {
+					
+				}
+			});
+		},
+		error:function() {}
+	})
+});
 
